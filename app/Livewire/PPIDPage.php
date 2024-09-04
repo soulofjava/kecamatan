dffa7d0690dd5ba5e8489be11dfe9020471c731b
isamaulanatantra <isamaulanatantra@gmail.com>
2024-09-04 07:24:30 +0700

    install livewire 3

---
+++ app/Livewire/PPIDPage.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire;
+
+use Livewire\Component;
+
+class PPIDPage extends Component
+{
+    public function render()
+    {
+        return view('livewire.p-p-i-d-page');
+    }
+}
